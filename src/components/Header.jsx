2b679ef06cdd3fc63f4627f8ea08ac420b04ddaf
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-09-08 12:14:31 +0545

    adminAddedinNavbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="w-full bg-black text-white">
+        <Link to="/admin">
+          <h1>Admin</h1>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
